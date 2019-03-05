01f637d7cdeb3e67b2169d606ba7b220a87d3d09
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2019-03-05 21:19:37 +0100

    Update dependencies

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-const express = require("express"),
-	path = require("path"),
+// eslint-disable-next-line no-unused-vars
+const dotenv = require("dotenv").config(),
+	bodyParser = require("body-parser"),
+	createError = require("http-errors"),
+	express = require("express"),
 	cookieParser = require("cookie-parser"),
 	logger = require("morgan"),
-	indexRouter = require("./routes/index"),
-	usersRouter = require("./routes/users"),
+	helmet = require("helmet"),
 	app = express();
 
+// CORS
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+// body-parser
+app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// routes
+app.use(require("./routes"));
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	next(createError(404, `Cannot ${req.method} ${req.originalUrl}`));
+});
+// error handler
+app.use((err, req, res) => {
+	res.status(err.status || 500);
+	res.send(err.message);
+});
 
 module.exports = app;
