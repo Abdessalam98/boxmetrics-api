d09262258c2a2f71b7f3fc9b804d1460a466abf2
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2019-03-05 21:05:37 +0100

    Add eslint

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express'),
-	path = require('path'),
-	cookieParser = require('cookie-parser'),
-	logger = require('morgan'),
-	indexRouter = require('./routes/index'),
-	usersRouter = require('./routes/users'),
+const express = require("express"),
+	path = require("path"),
+	cookieParser = require("cookie-parser"),
+	logger = require("morgan"),
+	indexRouter = require("./routes/index"),
+	usersRouter = require("./routes/users"),
 	app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
