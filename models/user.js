5611e21373aa8d9184a6c881ed9f15e57e8689a1
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2019-03-09 08:20:54 +0100

    make it better

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+
 module.exports = mongoose.model("User", {
 	firstName: {
 		type: String,
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,22 @@
 const router = require("express").Router(),
 	UserController = require("../../controllers/UserController"),
-	verifyToken = require("../../middleware/verifyToken");
+	jwt = require("jsonwebtoken"),
+	jwtSecret = require("../../config/jwt");
 
 // Middleware
-router.use(verifyToken).route("/").get(UserController.index);
+router
+	.use((req, res, next) => {
+		const token = req.headers["x-access-token"];
+		if (!token) return res.status(403).send({ auth: false, message: "No token provided" });
+
+		jwt.verify(token, jwtSecret, function(err, decoded) {
+			if (err) return res.status(401).send({ auth: false, message: "Failed to authenticate token" });
+			req.userId = decoded.id;
+			next();
+		});
+	})
+	.route("/")
+	.get(UserController.index);
 
 // router.get("/", verifyToken, function(req, res) {
 // 	res.status(200).json(null);
