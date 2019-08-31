2d1d3450c044d9d32cfb3569f296632f700c5614
Abdessalam BENHARIRA <belmehelabdessalam.benharira@ynov.com>
2019-08-31 10:09:01 +0200

    refactor: middleware jwt auth

--- routes/api/servers.js
+++ routes/api/servers.js
@@ -1,50 +1,15 @@
 const router = require("express").Router();
+const verifyToken = require("../../middleware/verifyToken");
 const ServerController = require("../../controllers/ServerController");
-const jwt = require("jsonwebtoken");
-const jwtSecret = require("../../config/jwt");
 
-// Middleware
 router
-	.use((req, res, next) => {
-		const token = req.headers["x-access-token"];
-		if (!token)
-			return res
-				.status(403)
-				.send({ auth: false, message: "No token provided" });
-
-		jwt.verify(token, jwtSecret, function(err, decoded) {
-			if (err)
-				return res.status(401).send({
-					auth: false,
-					message: "Failed to authenticate token"
-				});
-			req.userId = decoded.id;
-			next();
-		});
-	})
+	.use(verifyToken)
 	.route("/")
 	.get(ServerController.index)
 	.post(ServerController.store);
 
-// Middleware
 router
-	.use((req, res, next) => {
-		const token = req.headers["x-access-token"];
-		if (!token)
-			return res
-				.status(403)
-				.send({ auth: false, message: "No token provided" });
-
-		jwt.verify(token, jwtSecret, function(err, decoded) {
-			if (err)
-				return res.status(401).send({
-					auth: false,
-					message: "Failed to authenticate token"
-				});
-			req.userId = decoded.id;
-			next();
-		});
-	})
+	.use(verifyToken)
 	.route("/:id")
 	.get(ServerController.show)
 	.delete(ServerController.destroy);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,47 +1,14 @@
 const router = require("express").Router();
+const verifyToken = require("../../middleware/verifyToken");
 const UserController = require("../../controllers/UserController");
-const jwt = require("jsonwebtoken");
-const jwtSecret = require("../../config/jwt");
 
-// Middleware
 router
-	.use((req, res, next) => {
-		const token = req.headers["x-access-token"];
-		if (!token)
-			return res
-				.status(403)
-				.send({ auth: false, message: "No token provided" });
-
-		jwt.verify(token, jwtSecret, function(err, decoded) {
-			if (err)
-				return res.status(401).send({
-					auth: false,
-					message: "Failed to authenticate token"
-				});
-			req.userId = decoded.id;
-			next();
-		});
-	})
+	.use(verifyToken)
 	.route("/")
 	.get(UserController.index);
-router
-	.use((req, res, next) => {
-		const token = req.headers["x-access-token"];
-		if (!token)
-			return res
-				.status(403)
-				.send({ auth: false, message: "No token provided" });
 
-		jwt.verify(token, jwtSecret, function(err, decoded) {
-			if (err)
-				return res.status(401).send({
-					auth: false,
-					message: "Failed to authenticate token"
-				});
-			req.userId = decoded.id;
-			next();
-		});
-	})
+router
+	.use(verifyToken)
 	.route("/:id")
 	.get(UserController.show)
 	.put(UserController.update)
